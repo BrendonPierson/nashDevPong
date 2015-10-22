caf393bc6105f8ea8b76e36199e9b3c4ae6c0389
Brendon Pierson <bp@Brendons-MacBook-Pro.local>
2015-10-22 10:53:03 -0500

    Added doubles logging

--- app/controllers/DoublesMatchesCtrl.js
+++ app/controllers/DoublesMatchesCtrl.js
@@ -5,11 +5,14 @@ app.controller("DoublesMatchesCtrl",
   "$firebaseArray",
   function($scope, fb, $location, $firebaseArray) {
 
+    var ref = new Firebase("https://nashdev-pong.firebaseio.com");
+
     $scope.title="Doubles Matches";
 
     $scope.users = fb.getUsersArr();
 
     $scope.user = fb.getAuthObj().$getAuth().github;
+
 
 
 
@@ -25,14 +28,42 @@ app.controller("DoublesMatchesCtrl",
     $scope.addMatch = function(){
       $scope.newMatch.date = Date.now();
       $scope.newMatch.t1player1 = fb.getAuthObj().$getAuth().uid;
+
       $scope.rowCollection.$add($scope.newMatch).then(function(ref) {
         var id = ref.key();
         console.log("added record with id " + id); // returns location in the array
-        $scope.newMatch = {};   
-        // updateRanks();
+        updateRanks($scope.newMatch);
         updateTableData();
+        $scope.newMatch = {};   
       });   
     };
+
+    function updateRanks(match){
+      console.log("t1player1", match.t1player1);
+      var team1uid = match.t1player1 + "-" + match.t1player2;
+      var team2uid = match.t2player1 + "-" + match.t2player2;
+      console.log("team2uid" + team2uid);
+
+      ref.child('doublesTeams').child(team1uid).child('teamUid').set(team1uid);
+      ref.child('doublesTeams').child(team2uid).child('teamUid').set(team2uid);
+
+      if(match.t1score > match.t2score){
+        ref.child('doublesTeams').child(team1uid).child('wins').push(team2uid);
+        ref.child('doublesTeams').child(team2uid).child('losses').push(team1uid);
+      } else {
+        ref.child('doublesTeams').child(team1uid).child('losses').push(team2uid);
+        ref.child('doublesTeams').child(team2uid).child('wins').push(team1uid);
+      }
+    }
+
+    function checkForUser(uid){
+      if(_.find(users, 'uid', uid)){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
 
 
 
