b21653b3e72db6470036e3961d1ff0264da601ae
Brendon Pierson <brendonpierson@gmail.com>
2015-10-22 21:36:20 -0500

    Fixed broken links in Gruntfile

--- app/lib/Gruntfile.js
+++ app/lib/Gruntfile.js
@@ -2,28 +2,13 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    uglify: {
-      my_target: {
-        files: {
-          '../dist/main.min.js': ['../app/**/*.js', 'bower_components/angular/angular.js', 'bower_components/angular-route/angular-route.js', 'bower_components/lodash/lodash.js', 'bower_components/angular-animate/angular-animate.js', 'bower_components/angular-aria/angular-aria.js', 'bower_components/firebase/firebase.js', 'bower_components/angularfire/dist/angularfire.js', 'bower_components/angularfire/dist/angularfire.js', 'bower_components/angular-material/angular-material.js', 'bower_components/angular-smart-table/dist/smart-table.js']
-        }
-      }
-    },
-
-    copy: {
-      main: {
-        src: ['../partials/*', '../index.html',],
-        dest: '../dist/',
-      },
-    },
-
     jshint: {
-      files: ['../app/**/*.js']
+      files: ['../controllers/*.js', '../facotries/*.js', '../filters/*.js']
     },
     sass: {
       dist: {
         files: {
-          '../app/styles/main.css': '../sass/main.scss'
+          '../styles/main.css': '../sass/main.scss'
         }
       }
     },
@@ -42,5 +27,4 @@ module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('default', ['jshint', 'sass', 'watch']);
-  grunt.registerTask('build',['copy', 'sass', 'uglify']);
 };
